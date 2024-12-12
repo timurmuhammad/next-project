5666f30cf4b6b82bb9c5fa99420f21268cf8170d
timur <timurmuhammad1607@gmail.com>
2024-12-12 10:38:40 +0200

    rymsfgbdf

--- src/components/languages/index.jsx
+++ src/components/languages/index.jsx
@@ -36,10 +36,10 @@ export function Languages() {
 
 	const [locale, setLocale] = useLocalStorage('locale', 'EN');
 
-	const scrollPosition = JSON.parse(localStorage.getItem('scrollPosition') || '{}');
-	if (scrollPosition && scrollPosition.y !== undefined) {
-		window.scrollTo(scrollPosition.x, scrollPosition.y);
-	}
+	// const scrollPosition = JSON.parse(localStorage.getItem('scrollPosition') || '{}');
+	// if (scrollPosition && scrollPosition.y !== undefined) {
+	// 	window.scrollTo(scrollPosition.x, scrollPosition.y);
+	// }
 
 	useEffect(() => {
 			const prevLocaleRef = localStorage.getItem('prevLocale') || 'en'
@@ -48,16 +48,18 @@ export function Languages() {
 			console.log(prevLocaleRef, 'prevLocaleRef')
 			console.log(locale, 'locale')
 
-      translatePage(prevLocaleRef);
-
-			localStorage.setItem('prevLocale', locale);
-
 			const scrollPosition = JSON.parse(localStorage.getItem('scrollPosition') || '{}');
 			if (scrollPosition && scrollPosition.y !== undefined) {
 				window.scrollTo(scrollPosition.x, scrollPosition.y);
 			}
-			// Удалите сохранённую позицию после восстановления
 			localStorage.removeItem('scrollPosition');
+
+      translatePage(prevLocaleRef);
+
+			localStorage.setItem('prevLocale', locale);
+
+			// Удалите сохранённую позицию после восстановления
+
     
 	}, [pathname, locale]);
 
@@ -79,137 +81,139 @@ const translatePage = (prevLocaleRef) => {
 
 
   return (
-				<DropdownMenu modal={false} translate="no" lang="x-ignore">
-					<DropdownMenuTrigger asChild>
-						<div className={styles.body}>
-							<Image src={en} alt='icon' width={25} height={18}></Image>
-							<p>EN</p>
-						</div>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent className='w-[186px] my-[8px] mr-[16px]'>
-						<div className="flex  flex-wrap  justify-between gap-[16px] p-[16px] rounded-[6px] bg-neutral-50 border border-solid border-[#d9d9d9]">
-							<DropdownMenuItem onClick={() => setLocale('EN')} className={styles.lang}>
-								<Image
-									src={en}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">EN</p>
-							</DropdownMenuItem>
-								<DropdownMenuItem onClick={() => setLocale('NL')} className={styles.lang}>
+				<div translate="no" lang="x-ignore">
+					<DropdownMenu modal={false}>
+						<DropdownMenuTrigger asChild>
+							<div className={styles.body}>
+								<Image src={en} alt='icon' width={25} height={18}></Image>
+								<p>EN</p>
+							</div>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent className='w-[186px] my-[8px] mr-[16px]'>
+							<div className="flex  flex-wrap  justify-between gap-[16px] p-[16px] rounded-[6px] bg-neutral-50 border border-solid border-[#d9d9d9]">
+								<DropdownMenuItem onClick={() => setLocale('EN')} className={styles.lang}>
 									<Image
-										src={nl}
+										src={en}
 										width={25}
 										height={17}
 										alt='icon'
 										className="flex-grow-0 flex-shrink-0"
 									></Image>
-									<p className=" text-[16px]  text-black">NL</p>
+									<p className=" text-[16px]  text-black">EN</p>
 								</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setLocale('ES')} className={styles.lang}>
-								<Image
-									src={es}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">ES</p>
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setLocale('DE')} className={styles.lang}>
-								<Image
-									src={de}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">DE</p>
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setLocale('FR')} className={styles.lang}>
-								<Image
-									src={fr}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">FR</p>
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setLocale('PH')} className={styles.lang}>
-								<Image
-									src={pg}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">PH</p>
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setLocale('ZH')} className={styles.lang}>
-								<Image
-									src={zh}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">ZH</p>
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setLocale('JP')} className={styles.lang}>
-								<Image
-									src={jp}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">JP</p>
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setLocale('ID')} className={styles.lang}>
-								<Image
-									src={id}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">ID</p>
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setLocale('CZ')} className={styles.lang}>
-								<Image
-									src={cz}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">CZ</p>
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setLocale('HI')} className={styles.lang}>
-								<Image
-									src={hi}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">HI</p>
-							</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setLocale('HU')} className={styles.lang}>
-								<Image
-									src={hu}
-									width={25}
-									height={17}
-									alt='icon'
-									className="flex-grow-0 flex-shrink-0"
-								></Image>
-								<p className=" text-[16px]  text-black">HU</p>
-							</DropdownMenuItem>
-						</div>
-					</DropdownMenuContent>
-				</DropdownMenu>
+									<DropdownMenuItem onClick={() => setLocale('NL')} className={styles.lang}>
+										<Image
+											src={nl}
+											width={25}
+											height={17}
+											alt='icon'
+											className="flex-grow-0 flex-shrink-0"
+										></Image>
+										<p className=" text-[16px]  text-black">NL</p>
+									</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setLocale('ES')} className={styles.lang}>
+									<Image
+										src={es}
+										width={25}
+										height={17}
+										alt='icon'
+										className="flex-grow-0 flex-shrink-0"
+									></Image>
+									<p className=" text-[16px]  text-black">ES</p>
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setLocale('DE')} className={styles.lang}>
+									<Image
+										src={de}
+										width={25}
+										height={17}
+										alt='icon'
+										className="flex-grow-0 flex-shrink-0"
+									></Image>
+									<p className=" text-[16px]  text-black">DE</p>
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setLocale('FR')} className={styles.lang}>
+									<Image
+										src={fr}
+										width={25}
+										height={17}
+										alt='icon'
+										className="flex-grow-0 flex-shrink-0"
+									></Image>
+									<p className=" text-[16px]  text-black">FR</p>
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setLocale('PH')} className={styles.lang}>
+									<Image
+										src={pg}
+										width={25}
+										height={17}
+										alt='icon'
+										className="flex-grow-0 flex-shrink-0"
+									></Image>
+									<p className=" text-[16px]  text-black">PH</p>
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setLocale('ZH')} className={styles.lang}>
+									<Image
+										src={zh}
+										width={25}
+										height={17}
+										alt='icon'
+										className="flex-grow-0 flex-shrink-0"
+									></Image>
+									<p className=" text-[16px]  text-black">ZH</p>
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setLocale('JP')} className={styles.lang}>
+									<Image
+										src={jp}
+										width={25}
+										height={17}
+										alt='icon'
+										className="flex-grow-0 flex-shrink-0"
+									></Image>
+									<p className=" text-[16px]  text-black">JP</p>
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setLocale('ID')} className={styles.lang}>
+									<Image
+										src={id}
+										width={25}
+										height={17}
+										alt='icon'
+										className="flex-grow-0 flex-shrink-0"
+									></Image>
+									<p className=" text-[16px]  text-black">ID</p>
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setLocale('CZ')} className={styles.lang}>
+									<Image
+										src={cz}
+										width={25}
+										height={17}
+										alt='icon'
+										className="flex-grow-0 flex-shrink-0"
+									></Image>
+									<p className=" text-[16px]  text-black">CZ</p>
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setLocale('HI')} className={styles.lang}>
+									<Image
+										src={hi}
+										width={25}
+										height={17}
+										alt='icon'
+										className="flex-grow-0 flex-shrink-0"
+									></Image>
+									<p className=" text-[16px]  text-black">HI</p>
+								</DropdownMenuItem>
+								<DropdownMenuItem onClick={() => setLocale('HU')} className={styles.lang}>
+									<Image
+										src={hu}
+										width={25}
+										height={17}
+										alt='icon'
+										className="flex-grow-0 flex-shrink-0"
+									></Image>
+									<p className=" text-[16px]  text-black">HU</p>
+								</DropdownMenuItem>
+							</div>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
   )
 }
