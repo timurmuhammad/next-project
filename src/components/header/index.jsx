078fe16bab54f156df6e18da3edc451fade00767
timur <timurmuhammad1607@gmail.com>
2024-12-14 17:40:21 +0200

    tnzdbffb

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,121 +1,273 @@
-'use client';
+'use client'
 
 import styles from './header.module.scss';
 import { Logo } from '@/ui/logo';
-import { TranslatedLink } from '@/components/translatedLink';
-import { useState, useRef, useEffect } from 'react';
-import Image from 'next/image';
+import {productsType} from '../../types/products.ts';
+import { Suspense, useEffect, useState, useRef, useLayoutEffect } from "react";
 import burgerClose from '@/ui/icons/burger_close.svg';
 import burgerOpen from '@/ui/icons/burger_open.svg';
+import Image from 'next/image';
+import cn from 'classnames';
+import dashboard from '@/ui/icons/dashboard.svg';
+import {menuMobileType} from '../../types/menuMobile';
+import arrow from '@/ui/icons/arrow_up.svg';
+import polygon from '@/ui/icons/polygon.svg';
 import { Languages } from '../languages';
 import { SocialMedia } from '@/ui/socialMedia';
+import {TranslatedLink} from '@/components/translatedLink';
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useLocalStorage } from '@/hooks/useLocalStorage';
+
+
+
+const accordion = 'Products'
 
 export const Header = () => {
-    const [locale, setLocale] = useState(localStorage.getItem('locale') || 'EN');
-    const [burgerActive, setBurgerActive] = useState(false);
-    const lineRef = useRef(null);
-    const workerRef = useRef(null);
 
-    useEffect(() => {
-        const prevLocale = localStorage.getItem('prevLocale') || 'EN';
+	// const [md, setMd] = useState(false)
+  // const [lg, setLg] = useState(false)
 
-        if (prevLocale === locale) return;
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     const mdMatch = window.matchMedia("(max-width: 768px)")
+  //     const lgMatch = window.matchMedia("(max-width: 1024px)")
 
-        // Логика Worker в строке
-        const workerCode = `
-            self.onmessage = (event) => {
-                const { screenWidth, duration, fps } = event.data;
+  //     setMd(mdMatch.matches)
+  //     setLg(lgMatch.matches)
 
-                const frameTime = 1000 / fps; // Интервал между кадрами
-                const totalFrames = Math.round(duration / frameTime); // Количество кадров
-                const step = (screenWidth * 2) / totalFrames; // Изменение позиции за кадр
+  //     mdMatch.addEventListener('change', e => setMd(e.matches))
+  //     lgMatch.addEventListener('change', e => setLg(e.matches))
+  //   }
+  // }, [])
 
-                let x = -screenWidth; // Начальная позиция
-                let frame = 0;
 
-                const interval = setInterval(() => {
-                    if (frame >= totalFrames) {
-                        clearInterval(interval);
-                        self.postMessage({ done: true, x: screenWidth * 2 });
-                    } else {
-                        x += step;
-                        self.postMessage({ x });
-                        frame++;
-                    }
-                }, frameTime);
-            };
-        `;
 
-        // Создание Worker из строки
-        const blob = new Blob([workerCode], { type: 'application/javascript' });
-        workerRef.current = new Worker(URL.createObjectURL(blob));
 
-        const screenWidth = window.innerWidth;
+	const [locale, setLocale] = useLocalStorage('locale', 'EN');
+	const pathname = usePathname()
+	const lineRef = useRef(null);
 
-        workerRef.current.postMessage({
-            screenWidth,
-            duration: 1000,
-            fps: 50,
+	function runAnim(obj, data) {
+    const fpsdelay = 1000 / data.fps;
+    const { x_start: from, x_end: to, duration } = data;
+
+    let start = new Date().getTime(); // Начало анимации
+
+    function animate() {
+        const now = new Date().getTime() - start;
+        let progress = now / duration;
+
+        const elements = document.querySelectorAll("a, p, span, h1, h2, h3, h4, h5, h6");
+
+        // Добавляем blur ко всем подходящим элементам
+        elements.forEach((el) => {
+            if (
+                el.textContent.trim().length > 0 && // У элемента есть текст
+                !el.hasAttribute("no-translate") // У элемента нет атрибута no-translate
+            ) {
+                el.style.filter = `blur(5px)`; // Постоянный эффект blur
+                el.style.transition = "filter 0s";
+            }
         });
 
-        workerRef.current.onmessage = (event) => {
-            const { x, done } = event.data;
+        if (progress > 1.0) progress = 1;
 
-            if (lineRef.current) {
-                lineRef.current.style.transform = `translateX(${x}px)`;
-                lineRef.current.style.opacity = 1;
+        const newval = (to - from) * progress * progress + from;
+
+        obj.style.transform = `translate(${Math.round(newval)}px, 0)`;
+				obj.style.opacity = 1;
+
+
+        if (progress < 1) {
+            setTimeout(animate, fpsdelay); // Используем именованную функцию
+        } else {
+            // Проверяем условие после завершения круга
+						
+						
+            if (window.location.href.includes('_x_tr_hist=true')) {
+                // Снимаем blur, если условие выполнено
+                elements.forEach((el) => {
+                    if (
+                        el.textContent.trim().length > 0 &&
+                        !el.hasAttribute("no-translate")
+                    ) {
+                        el.style.filter = "none"; // Убираем эффект blur
+                    }
+                });
+            } else {
+                // Перезапускаем анимацию
+								obj.style.opacity = 0;
+								obj.style.transform = `translate(${from}px, 0)`;
+                start = new Date().getTime();
+                animate();
             }
+					
+        }
+    }
 
-            if (done) {
-                localStorage.setItem('prevLocale', locale);
-                workerRef.current.terminate();
-                workerRef.current = null;
-            }
-        };
+    animate(); // Запускаем анимацию
+}
 
-        return () => {
-            if (workerRef.current) {
-                workerRef.current.terminate();
-                workerRef.current = null;
-            }
-        };
-    }, [locale]);
 
-    const toggleBurgerMenu = () => {
-        setBurgerActive(!burgerActive);
-        document.body.style.overflow = burgerActive ? 'unset' : 'hidden';
-    };
+const isFirstRender = useRef(true);
 
-    return (
-        <div className={styles.body}>
-            <div className={styles.container + ' _container'}>
-                <div className={styles.burger} onClick={toggleBurgerMenu}>
-                    {burgerActive ? (
-                        <Image src={burgerClose} alt="icon" width={24} height={24} />
-                    ) : (
-                        <Image src={burgerOpen} alt="icon" width={24} height={24} />
-                    )}
-                </div>
+const elements = document.querySelectorAll("a, p, span, h1, h2, h3, h4, h5, h6");
+    
+if (isFirstRender.current) {
+	elements.forEach((el) => {
+		if (
+				el.textContent.trim().length > 0 && // У элемента есть текст
+				!el.hasAttribute("no-translate") // У элемента нет атрибута no-translate
+		) {
+				el.style.filter = `blur(5px)`; // Постоянный эффект blur
+				el.style.transition = "filter 0s";
+		}
+	});
+	isFirstRender.current = false
+}
+				
 
-                <Logo />
+let width
+	useLayoutEffect(() => {
+		
 
-                <div className={styles.auth}>
-                    <TranslatedLink href="/login">Login</TranslatedLink>
-                    <TranslatedLink href="/registration">Sign Up</TranslatedLink>
-                </div>
-            </div>
+		const prevLocale = localStorage.getItem('prevLocale') || 'en'
+		width = innerWidth()
+		if (lineRef.current) {
+			if (/*(prevLocale === locale && (prevLocale.toLowerCase() !== 'en' || locale.toLowerCase() !== 'en')) ||*/
+			(prevLocale !== locale)) {
+				console.log(true)
+				// if (window.location.href.includes('_x_tr_sl') && !window.location.href.includes('_x_tr_hist=true')) {
+					const screenWidth = window.innerWidth;
+					// console.log(screenWidth)
+					
+					runAnim(lineRef.current, {
+						fps: 50, 
+						x_start: -screenWidth,
+						x_end: screenWidth * 2, 
+						duration: 1000
+					})
+				// }
 
-            <div className={styles.progress}>
-                <div
-                    ref={lineRef}
-                    style={{
-                        width: '100vw',
-                        transform: 'translateX(-100vw)',
-                        opacity: 0,
-                    }}
-                    className={styles.line}
-                ></div>
-            </div>
-        </div>
-    );
-};
+			}
+		}
+	}, [/*pathname*/ locale])
+
+
+	const [ burgerActive, setBurgerActive ] = useState(false)
+	const [ category, setCategory] = useState('')
+	const [ open, setOpen ] =  useState(false)
+
+	function onChangeCategory() { 
+		category !== accordion ? setOpen(true) : setOpen(!open)
+	}
+
+	useEffect(() => {
+		if (typeof document !== 'undefined') {
+				if (burgerActive) {
+						document.body.style.overflow = 'hidden';
+				} else {
+						document.body.style.overflow = 'unset';
+				}
+		}   
+	}, [burgerActive]);
+
+	const innerWidth = () => {
+		return window.innerWidth
+	}
+
+
+	return <div className={cn(styles.body, {[styles.active]: burgerActive})}>
+		<div className={styles.container + ' _container'}>
+			<div className={styles.burger} onClick={() => setBurgerActive(!burgerActive)}>
+				{
+					burgerActive ? <Image src={burgerClose} alt='icon' width={24} height={24}></Image>
+					: <Image src={burgerOpen} alt='icon' width={24} height={24}></Image>
+				}
+			</div>
+
+			<Logo></Logo>
+
+			<ul className={styles.nav_desktop}>
+				<TranslatedLink  href='/about' className={styles.nav_desktop__item}>About</TranslatedLink >
+				<li className={cn(styles.nav_desktop__item, styles.popup_list)}>
+
+					<p 
+						className={cn(styles.popup_list__title, {[styles.active]: open})}>
+						<a>Products</a>
+						<Image src={polygon} alt='icon' width={10} height={8}></Image>
+					</p>
+
+					<ul className={cn(styles.popup_list__body, {[styles.active]: open}) }>
+						{ productsType.map( (listItem, index) => (
+							<TranslatedLink  href={listItem.link} key={index} className="flex relative gap-[24px] px-[30px] py-[16px] rounded-[6px]">
+								<Image src={listItem.icon} alt='icon' width={36} height={36}></Image>
+								<div className="flex flex-col justify-center">
+									<p className=" text-[16px]  text-black">
+										{listItem.name}
+									</p>
+									<p className=" text-[12px]  ">
+										{listItem.description}
+									</p>
+								</div>
+							</TranslatedLink >
+						))}
+					</ul>
+
+				</li>
+				<TranslatedLink  href='/security' className={styles.nav_desktop__item}>Security</TranslatedLink >
+				<TranslatedLink  href='/blog' className={styles.nav_desktop__item}>Blog</TranslatedLink >
+				<TranslatedLink  href='/helpcenter' className={styles.nav_desktop__item}>Help Hub</TranslatedLink >
+			</ul>
+
+			<ul className={cn(styles.nav_mobile, {[styles.active]: burgerActive})}>
+
+				<div>
+					{menuMobileType.map((item, index) => (
+						<div onClick={() => {
+							burgerActive && setCategory(item)
+						}}
+							key={index} 
+							className={(item === accordion ? styles.nav__accordion : styles.nav_mobile__item)}>
+								{item === accordion ? <div className={cn(styles.nav_accordion__body, {[styles.active]: category === item && open})}>
+									
+									<p onClick={() => onChangeCategory()} className={styles.nav_accordion__title}>
+										<a>{item}</a>
+										<Image src={arrow} alt='icon' width={14} height={8}></Image>
+									</p>
+	
+									<ul className={styles.nav_accordion__list}>
+										{productsType.map((item, index) => (
+											<p className={styles.nav_accordion__item} key={index}>{item.name}</p>
+										))}
+									</ul>
+	
+								</div> : <p onClick={() => onChangeCategory()} className={cn(styles.nav_mobile__text, {[styles.active]: category === item})}>{item}</p>}
+						</div>
+					))}
+				</div>
+
+				<div className={styles.nav_mobile__icons}>
+				<Suspense fallback={<div>Загрузка...</div>}>
+				<Languages></Languages>
+			</Suspense>
+					
+					<SocialMedia></SocialMedia>
+				</div>
+			</ul>
+
+			<div className={styles.auth}>
+				<button className={styles.dashboard}><Image src={dashboard} alt='icon' width={21} height={20}></Image></button>
+				<TranslatedLink  href='/login' className={styles.login}><p>Login</p></TranslatedLink >
+				<TranslatedLink  href='/registration' className={styles.sign_up}>Sign Up</TranslatedLink >
+			</div>
+		</div>
+
+		<div className={styles.progress}>
+			<div ref={lineRef} style={{ width: `${width}px`, transform: `translateX(-${width}px)`, opacity: 0 }} className={styles.line}></div>
+		</div>
+	</div>
+}
+
+
+
