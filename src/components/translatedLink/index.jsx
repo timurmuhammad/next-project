1a5e5acafa37322d1916dcde2c784fe9e6275b7a
timur <timurmuhammad1607@gmail.com>
2024-12-16 11:28:46 +0200

    srdbzdfb

--- src/components/translatedLink/index.jsx
+++ src/components/translatedLink/index.jsx
@@ -4,7 +4,7 @@ import { getLocalStorage } from '@/hooks/getLocalStorage'
 export const TranslatedLink = ({ href, children, locale = getLocalStorage('locale') || 'en', ...props }) => {
   const baseUrl = 'https://react-project-zdxg.vercel.app';
 
-  const translatedHref = `https://translate.google.com/translate?hl=${locale}&sl=en&tl=${locale}&u=${encodeURIComponent(
+  const translatedHref = `https://translate.google.com/translate?hl=en&sl=${locale}&tl=${locale}&u=${encodeURIComponent(
     `${baseUrl}${href}`
   )}`;
 
