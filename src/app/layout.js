1a34af0e551e47450ca4731abc070b1a346d1bd5
timur <timurmuhammad1607@gmail.com>
2024-12-20 14:28:33 +0200

    stndbzfdbdfb

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,13 @@ import { sofiaPro, inter } from "../ui/fonts/index"
 import "@/ui/globals.scss";
 import { LocaleProvider } from '@/global/locale';
 import { Suspense } from "react";
-
+import { Header } from "@/components/header";
 
 export default function RootLayout({ children }) {
   return (
       <html lang="en">
         <body className={sofiaPro.className}>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<Header></Header>}>
             <LocaleProvider>
               {children}
             </LocaleProvider>
