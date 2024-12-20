a6ee8dbe045ea01ff04931aa83922bff5947cf3a
timur <timurmuhammad1607@gmail.com>
2024-12-20 13:58:23 +0200

    tnzdbfdb

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,15 +2,20 @@
 
 import { sofiaPro, inter } from "../ui/fonts/index"
 import "@/ui/globals.scss";
-// import { LocaleProvider } from '@/global/locale';
+import { LocaleProvider } from '@/global/locale';
+import { Suspense } from "react";
 
 
 export default function RootLayout({ children }) {
   return (
-    // <LocaleProvider>
       <html lang="en">
-        <body className={sofiaPro.className}>{children}</body>
+        <body className={sofiaPro.className}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <LocaleProvider>
+              {children}
+            </LocaleProvider>
+          </Suspense>
+          </body>
       </html>
-    // </LocaleProvider>
   );
 }
